92c9061f7e395b3375a58e440905d2ccfef0a678
Subash B <subash59245@gmail.com>
2025-12-26 21:19:57 +0530

    Update auth.services.js

--- backend/src/services/auth.services.js
+++ backend/src/services/auth.services.js
@@ -1,43 +1,47 @@
 const User = require("../models/user.model");
 const bcrypt = require("bcryptjs");
-const {generateToken} = require("../middleware/auth.middleware");
-const dotenv = require("dotenv");
-const jwt = require("jsonwebtoken")
-
-dotenv.config();
+const { generateToken } = require("../middleware/auth.middleware");
+const jwt = require("jsonwebtoken");
 
 exports.registerUser = async (req, res) => {
   const { name, email, password } = req.body;
-  console.log("request Recieved")
+  console.log("Request Received");
+
   const existingUser = await User.findOne({ email });
   if (existingUser) {
     return res.status(400).json({ message: "User already exists" });
   }
+
   const hashedPassword = await bcrypt.hash(password, 10);
   const newUser = new User({ name, email, password: hashedPassword });
   await newUser.save();
-  res
-    .status(201)
-    .json({
-      message: "User registered successfully",
-    });
+
+  res.status(201).json({ message: "User registered successfully" });
 };
 
 exports.loginUser = async (req, res) => {
   const { email, password } = req.body;
+
   const user = await User.findOne({ email });
   if (!user) {
     return res.status(400).json({ message: "Invalid credentials" });
   }
+
   const isMatch = await bcrypt.compare(password, user.password);
   if (!isMatch) {
     return res.status(400).json({ message: "Invalid credentials" });
   }
+
   const token = generateToken(user._id);
+
   res.cookie("token", token, {
     httpOnly: true,
     secure: true,
+    sameSite: "none",
+    maxAge: 1000 * 60 * 60 * 24, // 1 day
+    path: "/",
   });
+
   res.status(200).json({ message: "Login successful", token });
 };
 
@@ -45,18 +49,23 @@ exports.logoutUser = async (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
     secure: true,
-    sameSite: "lax",
+    sameSite: "none",
     path: "/",
   });
 
   res.status(200).json({ message: "Logout successful" });
-}
+};
 
 exports.verifyUser = async (req, res) => {
-  const token = req.cookies.token
+  const token = req.cookies.token;
   if (!token) {
-    return res.status(401).json({ authenticated: false })
+    return res.status(401).json({ authenticated: false });
   }
-  jwt.verify(token, process.env.JWT_SECRET)
-  return res.status(200).json({ authenticated: true })
-}
+
+  try {
+    jwt.verify(token, process.env.JWT_SECRET);
+    return res.status(200).json({ authenticated: true });
+  } catch (err) {
+    return res.status(401).json({ authenticated: false });
+  }
+};
